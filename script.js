0f21644a8076fec824010084afb24a4737e2cdd1
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-23 12:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+const nameInput = document.getElementById("name");
+const yearInput = document.getElementById("year");
+const button = document.getElementById("button");
+const url = document.getElementById("url");
+
+button.addEventListener("click", function() {
+  const name = encodeURIComponent(nameInput.value);
+  const year = encodeURIComponent(yearInput.value);
+  const queryString = `?name=${name}&year=${year}`;
+  url.textContent = `https://localhost:8080/${queryString}`;
+});
